dd23624505b7b638d324e06b6baee18885ff8b54
Tanganelli <giacomo.tanganelli@for.unipi.it>
2014-05-02 13:32:16 +0200

    Refactoring

--- coapthon2/layers/reliability.py
+++ coapthon2/layers/reliability.py
@@ -1,15 +1,17 @@
-from coapthon2.layers.layer import Layer
 from coapthon2.layers.matcher import Matcher
 from coapthon2.messages.request import Request
 
 __author__ = 'Giacomo Tanganelli'
 
 
-class Reliability(Layer):
+class Reliability(object):
+    def __init__(self, server):
+        self._server = server
+
     def handle_request(self, request):
         assert isinstance(request, Request)
         if request.duplicated:
-            matcher = self.layer_stack.next
+            matcher = self._server.layer_stack.next
             assert isinstance(matcher, Matcher)
             response = matcher.response_sent(request.mid)
             #assert isinstance(request, Response)
