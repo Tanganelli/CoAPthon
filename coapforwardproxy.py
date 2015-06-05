e2fb5cb7026b1599dc734c31fe91b8c60102fb96
Giacomo Tanganelli <giacomo.tanganelli@for.unipi.it>
2015-06-05 11:32:38 +0200

    ipv6 binding

--- coapforwardproxy.py
+++ coapforwardproxy.py
@@ -12,19 +12,19 @@ class CoAPForwardProxy(ProxyCoAP):
 
 
 def main():
-    portSocket = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
-    # Make the port non-blocking and start it listening.
-    portSocket.setblocking(False)
-    portSocket.bind(('bbbb::2', 5683))
+    # portSocket = socket.socket(socket.AF_INET6, socket.SOCK_DGRAM)
+    # # Make the port non-blocking and start it listening.
+    # portSocket.setblocking(False)
+    # portSocket.bind(('bbbb::2', 5683))
+    #
+    # # Now pass the port file descriptor to the reactor
+    # port = reactor.adoptDatagramPort(
+    #     portSocket.fileno(), socket.AF_INET6, CoAPForwardProxy("bbbb::2", 5683))
+    #
+    # # The portSocket should be cleaned up by the process that creates it.
+    # portSocket.close()
 
-    # Now pass the port file descriptor to the reactor
-    port = reactor.adoptDatagramPort(
-        portSocket.fileno(), socket.AF_INET6, CoAPForwardProxy("bbbb::2", 5683))
-
-    # The portSocket should be cleaned up by the process that creates it.
-    portSocket.close()
-
-    #reactor.listenUDP(5683, CoAPForwardProxy("bbbb::2", 5683), "bbbb::2")
+    reactor.listenUDP(5683, CoAPForwardProxy("bbbb::2", 5683), "::")
     reactor.run()
 
 
