9039ea775f6cba2e922ab7c87db5d1b3229308c0
Giacomo Tanganelli <giacomo.tanganelli@for.unipi.it>
2015-06-05 20:14:25 +0200

    debug

--- coapthon/client/coap_protocol.py
+++ coapthon/client/coap_protocol.py
@@ -569,7 +569,7 @@ class HelperClient(object):
 
     def start(self, operations):
         self.protocol.set_operations(operations)
-        reactor.listenUDP(0, self.protocol, interface="127.0.0.1")
+        reactor.listenUDP(0, self.protocol, interface="bbbb::2")
         try:
             reactor.run()
         except twisted.internet.error.ReactorAlreadyRunning:
