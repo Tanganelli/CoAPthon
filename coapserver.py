d1a9f2cedfbb2ee2bfe550c56537a87c6abb9d6c
Giacomo Tanganelli <giacomo.tanganelli@for.unipi.it>
2015-06-12 00:39:04 +0200

    coap server ipv6

--- coapserver.py
+++ coapserver.py
@@ -12,18 +12,18 @@ class CoAPServer(CoAP):
     def __init__(self, host, port, multicast=False):
         CoAP.__init__(self, multicast)
         self.add_resource('basic/', BasicResource())
-        self.add_resource('storage/', Storage())
-        self.add_resource('separate/', Separate())
-        self.add_resource('long/', Long())
-        self.add_resource('big/', Big())
+        # self.add_resource('storage/', Storage())
+        # self.add_resource('separate/', Separate())
+        # self.add_resource('long/', Long())
+        # self.add_resource('big/', Big())
         print "CoAP Server start on " + host + ":" + str(port)
-        print(self.root.dump())
+        # print(self.root.dump())
 
 
 def main():
-    server = CoAPServer("127.0.0.1", 5683)
+    server = CoAPServer("bbbb::5", 5683)
     #reactor.listenMulticast(5683, server, listenMultiple=True)
-    reactor.listenUDP(5683, server, "127.0.0.1")
+    reactor.listenUDP(5683, server, "bbbb::5")
     reactor.run()
 
 
