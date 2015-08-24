29dcaee5b54b9439d43e7f1b9b8f88be1f59b3c3
Giacomo Tanganelli <giacomo.tanganelli@for.unipi.it>
2015-08-24 14:44:02 +0200

    coapserver

--- coapserver.py
+++ coapserver.py
@@ -1,51 +1,31 @@
 #!/bin/python
-import getopt
-import sys
+from twisted.internet import reactor
+from coapthon import defines
 from coapthon.server.coap_protocol import CoAP
 from example_resources import Storage, Separate, BasicResource, Long, Big
+
+import twisted.internet.base
+twisted.internet.base.DelayedCall.debug = True
 
 
 class CoAPServer(CoAP):
     def __init__(self, host, port, multicast=False):
-        CoAP.__init__(self, (host, port), multicast)
+        CoAP.__init__(self, multicast)
         self.add_resource('basic/', BasicResource())
         self.add_resource('storage/', Storage())
         self.add_resource('separate/', Separate())
         self.add_resource('long/', Long())
         self.add_resource('big/', Big())
         print "CoAP Server start on " + host + ":" + str(port)
-        print self.root.dump()
+        # print(self.root.dump())
 
 
-def usage():
-    print "coapserver.py -i <ip address> -p <port>"
+def main():
+    server = CoAPServer("127.0.0.1", 5683)
+    # reactor.listenMulticast(5683, server, listenMultiple=True)
+    reactor.listenUDP(5683, server, "127.0.0.1")
+    reactor.run()
 
 
-def main(argv):
-    ip = "127.0.0.1"
-    port = 5683
-    try:
-        opts, args = getopt.getopt(argv, "hi:p:", ["ip=", "port="])
-    except getopt.GetoptError:
-        usage()
-        sys.exit(2)
-    for opt, arg in opts:
-        if opt == '-h':
-            usage()
-            sys.exit()
-        elif opt in ("-i", "--ip"):
-            ip = arg
-        elif opt in ("-p", "--port"):
-            port = int(arg)
-
-    server = CoAPServer(ip, port)
-    try:
-        server.listen(10)
-    except KeyboardInterrupt:
-        print "Server Shutdown"
-        server.close()
-        print "Exiting..."
-
-
-if __name__ == "__main__":
-    main(sys.argv[1:])
+if __name__ == '__main__':
+    main()
