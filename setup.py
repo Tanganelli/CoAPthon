57872e438a8ca07524a02438742c0307c2e6dad8
Tanganelli <giacomo.tanganelli@for.unipi.it>
2015-10-26 15:02:20 +0100

    setup

--- setup.py
+++ setup.py
@@ -3,13 +3,12 @@ from distutils.core import setup
 setup(
     name='CoAPthon',
     version='3.0.0',
-    packages=['test', 'coapthon', 'coapthon.layers', 'coapthon.client', 'coapthon.server', 'coapthon.messages',
-              'coapthon.resources'],
+    packages=['test', 'coapthon', 'coapthon.layers', 'coapthon.client', 'coapthon.server', 'coapthon.messages'],
     url='https://github.com/Tanganelli/CoAPthon',
     license='MIT License',
     author='Giacomo Tanganelli',
     author_email='giacomo.tanganelli@for.unipi.it',
     description='CoAPthon is a python library to the CoAP protocol. ',
-    scripts=['coapserver.py', 'coapclient.py', 'example_resources.py', 'coapforwardproxy.py', 'coapreverseproxy.py',
-             'reverse_proxy_mapping.xml'], requires=['sphinx', 'futures']
+    scripts=['coapserver.py', 'coapclient.py', 'exampleresources.py', 'coapforwardproxy.py', 'coapreverseproxy.py',
+             'reverse_proxy_mapping.xml'], requires=['sphinx']
 )
