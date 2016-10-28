eb67067cbb284d60b1a801ccbdb6cdfab5702efe
Emilio Vallati <valla.emilio@gmail.com>
2016-10-28 17:43:35 +0200

    Add files via upload

---
+++ coapthon/caching/cacheexceptions.py
@@ -0,0 +1,19 @@
+
+
+"""
+exception used to signal a full cache
+"""
+
+
+class FullCacheException:
+    def __init__(self):
+        return
+
+"""
+exception used to signal an empty cache
+"""
+
+
+class EmptyCacheException:
+    def __init__(self):
+        return
---
+++ coapthon/caching/coapcache.py
@@ -0,0 +1,47 @@
+__author__ = 'Emilio Vallati'
+
+class CoapCache:
+    def __init__(self, max_dim):
+        """
+
+        :param max_dim:
+        """
+        self.cache = None
+
+    def update(self, key, element):
+        """
+
+        :param key:
+        :param element:
+        :return:
+        """
+        raise NotImplementedError
+
+    def get(self, key):
+        """
+
+        :param key:
+        :return: CacheElement
+        """
+        raise NotImplementedError
+
+    def is_full(self):
+        """
+
+        :return:
+        """
+        raise NotImplementedError
+
+    def is_empty(self):
+        """
+
+        :return:
+        """
+        raise NotImplementedError
+
+    def debug_print(self):
+        """
+
+        :return:
+        """
+        raise NotImplementedError
