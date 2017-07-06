b6983fbf48399bc5687656be55ac5b9cce4f4718
giacomo.tanganelli@for.unipi.it <hitachicm500et>
2017-07-06 13:34:53 +0200

    coverage

--- coverage_test.py
+++ coverage_test.py
@@ -222,82 +222,82 @@ class Tests(unittest.TestCase):
         self.current_mid += 1
         self._test_with_client([exchange1, exchange2, exchange3, exchange4])
 
-    def test_separate(self):
-        print "TEST_SEPARATE"
-        path = "/separate"
-        req = Request()
-        req.code = defines.Codes.GET.number
-        req.uri_path = path
-        req.type = defines.Types["CON"]
-        req._mid = self.current_mid
-        req.destination = self.server_address
-
-        expected = Response()
-        expected.type = defines.Types["CON"]
-        expected._mid = None
-        expected.code = defines.Codes.CONTENT.number
-        expected.token = None
-        expected.max_age = 60
-
-        exchange1 = (req, expected)
-
-        self.current_mid += 1
-
-        req = Request()
-        req.code = defines.Codes.POST.number
-        req.uri_path = path
-        req.type = defines.Types["CON"]
-        req._mid = self.current_mid
-        req.destination = self.server_address
-        req.payload = "POST"
-
-        expected = Response()
-        expected.type = defines.Types["CON"]
-        expected._mid = None
-        expected.code = defines.Codes.CHANGED.number
-        expected.token = None
-        expected.options = None
-
-        exchange2 = (req, expected)
-
-        self.current_mid += 1
-
-        req = Request()
-        req.code = defines.Codes.PUT.number
-        req.uri_path = path
-        req.type = defines.Types["CON"]
-        req._mid = self.current_mid
-        req.destination = self.server_address
-        req.payload = "PUT"
-
-        expected = Response()
-        expected.type = defines.Types["CON"]
-        expected._mid = None
-        expected.code = defines.Codes.CHANGED.number
-        expected.token = None
-        expected.options = None
-
-        exchange3 = (req, expected)
-
-        self.current_mid += 1
-
-        req = Request()
-        req.code = defines.Codes.DELETE.number
-        req.uri_path = path
-        req.type = defines.Types["CON"]
-        req._mid = self.current_mid
-        req.destination = self.server_address
-
-        expected = Response()
-        expected.type = defines.Types["CON"]
-        expected._mid = None
-        expected.code = defines.Codes.DELETED.number
-        expected.token = None
-
-        exchange4 = (req, expected)
-
-        self.current_mid += 1
-        self._test_with_client([exchange1, exchange2, exchange3, exchange4])
+    # def test_separate(self):
+    #     print "TEST_SEPARATE"
+    #     path = "/separate"
+    #     req = Request()
+    #     req.code = defines.Codes.GET.number
+    #     req.uri_path = path
+    #     req.type = defines.Types["CON"]
+    #     req._mid = self.current_mid
+    #     req.destination = self.server_address
+    #
+    #     expected = Response()
+    #     expected.type = defines.Types["CON"]
+    #     expected._mid = None
+    #     expected.code = defines.Codes.CONTENT.number
+    #     expected.token = None
+    #     expected.max_age = 60
+    #
+    #     exchange1 = (req, expected)
+    #
+    #     self.current_mid += 1
+    #
+    #     req = Request()
+    #     req.code = defines.Codes.POST.number
+    #     req.uri_path = path
+    #     req.type = defines.Types["CON"]
+    #     req._mid = self.current_mid
+    #     req.destination = self.server_address
+    #     req.payload = "POST"
+    #
+    #     expected = Response()
+    #     expected.type = defines.Types["CON"]
+    #     expected._mid = None
+    #     expected.code = defines.Codes.CHANGED.number
+    #     expected.token = None
+    #     expected.options = None
+    #
+    #     exchange2 = (req, expected)
+    #
+    #     self.current_mid += 1
+    #
+    #     req = Request()
+    #     req.code = defines.Codes.PUT.number
+    #     req.uri_path = path
+    #     req.type = defines.Types["CON"]
+    #     req._mid = self.current_mid
+    #     req.destination = self.server_address
+    #     req.payload = "PUT"
+    #
+    #     expected = Response()
+    #     expected.type = defines.Types["CON"]
+    #     expected._mid = None
+    #     expected.code = defines.Codes.CHANGED.number
+    #     expected.token = None
+    #     expected.options = None
+    #
+    #     exchange3 = (req, expected)
+    #
+    #     self.current_mid += 1
+    #
+    #     req = Request()
+    #     req.code = defines.Codes.DELETE.number
+    #     req.uri_path = path
+    #     req.type = defines.Types["CON"]
+    #     req._mid = self.current_mid
+    #     req.destination = self.server_address
+    #
+    #     expected = Response()
+    #     expected.type = defines.Types["CON"]
+    #     expected._mid = None
+    #     expected.code = defines.Codes.DELETED.number
+    #     expected.token = None
+    #
+    #     exchange4 = (req, expected)
+    #
+    #     self.current_mid += 1
+    #     self._test_with_client([exchange1, exchange2, exchange3, exchange4])
 
     def test_post(self):
         print "TEST_POST"
