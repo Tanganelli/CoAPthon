25079b14534eadbe5e85aae9a01ef2c7102d6dd1
ens33 <carmelo.aparo.ca@gmail.com>
2018-04-21 19:16:36 +0200

    Redirect /rd-lookup/res and /rd-lookup/ep to /rd-lookup

--- coapthon/layers/requestlayer.py
+++ coapthon/layers/requestlayer.py
@@ -55,6 +55,8 @@ class RequestLayer(object):
         path = str("/" + transaction.request.uri_path)
         if path.startswith("/rd/"):
             path = "/rd"
+        elif (path == "/rd-lookup/res") or (path == "/rd-lookup/ep"):
+            path = "/rd-lookup"
         transaction.response = Response()
         transaction.response.destination = transaction.request.source
         transaction.response.token = transaction.request.token
